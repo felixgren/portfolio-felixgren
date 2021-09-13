33d4f95d188963ddf56a4c44efffc74e5fe128e0
felixgren <33127919+felixgren@users.noreply.github.com>
2021-09-13 17:04:29 +0200

    Add todo notes

---
+++ todo.js
@@ -0,0 +1,31 @@
+<PortfolioMVP>
+  <Models>
+    <Search>Find all models</Search>
+    <Code>Import models into three</Code>
+    <Code>Optimize for web</Code>
+  </Models>
+
+  <ProjectInfo>
+    <Design>Design layout</Design>
+    <Write>Add text</Write>
+    <Code>Hook up to model hover/click</Code>
+    <Think>Figure out transition</Think>
+  </ProjectInfo>
+
+  <Hero>
+    <Think>Figure out what to do with HEYAYAY!</Think>
+    <Fix>Make phase 2 start sooner</Fix>
+  </Hero>
+
+  <AboutMe>
+    <Code>Create knowledge and skills section</Code>
+    <Write>Add 'about me' text </Write>
+    <Record>Record new video with kinect</Record>
+    <Code>Implement kinect video</Code>
+  </AboutMe>
+
+  <General>
+    <Fix>Resolve camera breaking from scrollwheel jumping</Fix>
+    <Code>Verify mobile support</Code>
+  </General>
+</PortfolioMVP>;
