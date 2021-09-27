026759f54fc2efa1da4e6ccb0db29755e7e9760e
felixgren <33127919+felixgren@users.noreply.github.com>
2021-09-27 20:20:22 +0200

    Update TODO tasks

--- todo.js
+++ todo.js
@@ -1,39 +1,65 @@
+// Todo
 <PortfolioMVP>
+  <General>
+    <todo>
+      <Audio>Compress audio file & verify performance/remove preload</Audio>
+      <States>Add state management, this is a mess</States>
+      <Fix>Resolve camera breaking from scrollwheel jumping</Fix>
+      <Mobile>Fix whitespace/UI from search bar hiding on scroll</Mobile>
+      <Optimize>Resolve stutter/jank on camera entering stage 2</Optimize>
+    </todo>
+  </General>
+
   <Models>
-    <Search>Find all models</Search>
-    <Code>Import models into three</Code>
-    <Code>Optimize for web</Code>
+    <todo>
+      <Code>Optimize for web, KTX compression?</Code>
+      <Refactor>Break all models into individual files</Refactor>
+      <Refactor>Move camera logic to its own component</Refactor>
+    </todo>
   </Models>
 
   <ProjectInfo>
-    <Done>
-      <Design>Design layout</Design>
-      <Write>Add text</Write>
-      <Code>Hook up to model hover/click</Code>
-      <Think>Figure out transition</Think>
-    </Done>
+    <todo></todo>
   </ProjectInfo>
 
   <Hero>
-    <Fix>Make phase 2 start sooner</Fix>
-    <Done>
-      <Think>Figure out what to do with HEYAYAY!</Think>
-    </Done>
+    <todo>
+      <Fix>Make phase 2 start sooner</Fix>
+    </todo>
   </Hero>
 
   <AboutMe>
-    <Write>Add 'about me' text </Write>
-    <Record>Record new video with kinect</Record>
-    <Code>Implement kinect video</Code>
-    <Done>
-      <Code>Create knowledge and skills section</Code>
-    </Done>
+    <todo>
+      <Write>Add 'about me' text </Write>
+      <Record>Record new video with kinect</Record>
+      <Code>Implement kinect video</Code>
+    </todo>
   </AboutMe>
-
-  <General>
-    <Fix>Resolve camera breaking from scrollwheel jumping</Fix>
-    <Code>Verify mobile support</Code>
-    <Audio>Compress audio file & verify performance/remove preload</Audio>
-    <States>Add state management, this is a mess</States>
-  </General>
 </PortfolioMVP>;
+
+// Completed Tasks
+<CompletedTasks>
+  <General>
+    <Code>Verify mobile support</Code>
+  </General>
+
+  <Models>
+    <Search>Find all models</Search>
+    <Code>Import models into three</Code>
+  </Models>
+
+  <ProjectInfo>
+    <Design>Design layout</Design>
+    <Write>Add text</Write>
+    <Code>Hook up to model hover/click</Code>
+    <Think>Figure out transition</Think>
+  </ProjectInfo>
+
+  <Hero>
+    <Think>Figure out what to do with HEYAYAY!</Think>
+  </Hero>
+
+  <AboutMe>
+    <Code>Create knowledge and skills section</Code>
+  </AboutMe>
+</CompletedTasks>;
